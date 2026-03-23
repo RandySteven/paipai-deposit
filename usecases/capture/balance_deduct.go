367b8b2a0e06f8a6ddfc82d6a7393d14cd1763a9
Randy Steven <randysteven12@gmail.com>
2026-03-23 22:47:00 +0700

    capture add logic

--- usecases/capture/balance_deduct.go
+++ usecases/capture/balance_deduct.go
@@ -1,7 +1,21 @@
 package capture
 
-import "context"
+import (
+	"context"
+
+	"github.com/RandySteven/paipai-deposit/apperror"
+)
 
 func (c *captureWorkflow) balanceDeduct(ctx context.Context, executionData *ExecutionData) (*ExecutionData, error) {
+	balance, err := c.balanceRepository.FindByID(ctx, executionData.TransactionHistory.BalanceID)
+	if err != nil {
+		return nil, apperror.NewCustomError(apperror.ErrInternalServer, "failed to find balance", err)
+	}
+
+	balance.BalanceAmount -= executionData.TransactionHistory.TransactionAmount
+	_, err = c.balanceRepository.Update(ctx, balance)
+	if err != nil {
+		return nil, apperror.NewCustomError(apperror.ErrInternalServer, "failed to update balance", err)
+	}
 	return executionData, nil
 }
--- usecases/capture/workflow.go
+++ usecases/capture/workflow.go
@@ -2,12 +2,23 @@ package capture
 
 import (
 	"context"
+	"time"
 
 	"github.com/RandySteven/paipai-deposit/apperror"
 	"github.com/RandySteven/paipai-deposit/entities/payloads/requests"
 	"github.com/RandySteven/paipai-deposit/entities/payloads/responses"
 	repository_interfaces "github.com/RandySteven/paipai-deposit/interfaces/repositories"
 	temporal_client "github.com/RandySteven/paipai-deposit/pkg/temporal"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/workflow"
+)
+
+const (
+	captureActivityCheckTransactionHistory = "check_transaction_history_authorized"
+	captureActivityBalanceDeduct           = "balance_deduct"
+	captureActivityUpdateTransactionStatus = "update_transaction_status"
+
+	sgNoNeed = ""
 )
 
 type (
@@ -24,6 +35,23 @@ type (
 )
 
 func (c *captureWorkflow) registerActivitiesAndWorkflows() {
+	activityOptions := &workflow.ActivityOptions{
+		ScheduleToCloseTimeout: 10 * time.Second,
+		StartToCloseTimeout:    10 * time.Second,
+		HeartbeatTimeout:       10 * time.Second,
+		RetryPolicy: &temporal.RetryPolicy{
+			InitialInterval:    10 * time.Second,
+			BackoffCoefficient: 2.0,
+			MaximumInterval:    10 * time.Second,
+			MaximumAttempts:    3,
+		},
+	}
+
+	c.workflowExecution.AddTransitionActivityWithOptions(captureActivityCheckTransactionHistory, sgNoNeed, c.checkTransactionStatus, activityOptions, captureActivityBalanceDeduct)
+	c.workflowExecution.AddTransitionActivityWithOptions(captureActivityBalanceDeduct, sgNoNeed, c.balanceDeduct, activityOptions, captureActivityUpdateTransactionStatus)
+	c.workflowExecution.AddTransitionActivityWithOptions(captureActivityUpdateTransactionStatus, sgNoNeed, c.updateTransactionStatus, activityOptions)
+
+	c.workflowExecution.RegisterWorkflow("Capture", c.capture)
 }
 
 func (c *captureWorkflow) Capture(ctx context.Context, request *requests.CaptureRequest) (response *responses.TransferResponse, appError *apperror.CustomError) {
